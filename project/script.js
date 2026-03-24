899acac2aac42fa78a04fa97798e25134ce8e2e7
Clemens <clemens.pueringer@students.htl-leonding.ac.at>
2026-03-24 15:30:16 +0100

    Basic functions für die Erstellung von Flipbooks (für die Library)

---
+++ project/script.js
@@ -0,0 +1,39 @@
+let book = document.getElementsByClassName("flipbook")[0];
+
+function resetBook() {
+    $(book).turn('destroy').html('');
+    // Startcover Hinzufügen
+    let covers = `
+    <div class="hard"><div id="cover"><div id="header">Das Zauberbuch des Magiers</div><img src="./media/img/dnd_logo.png" alt=""></div></div>
+    <div class="hard"></div>
+    <div class="hard"></div>
+    <div class="hard"><img src="./media/matt.png" alt="" style="width: 100%;"></div>
+    `
+    book.innerHTML = covers;
+        // turn.js initialisieren
+    $(book).turn();
+}
+function addPages(content1, content2) {
+    let totalPages = $(book).turn('pages') || $(book).children().length;
+    if (!totalPages || totalPages < 2) {
+        // noch kein Endcover / zu wenig Seiten -> normal hinzufügen
+        $(book).turn('addPage', `<div>${content}</div>`);
+        return;
+    }
+
+    // Endcover beibehalten und später wieder anfügen:
+    let backCoverIndex = totalPages;
+    let priorCoverIndex = totalPages - 1;
+
+    // Vor dem Endcover einfügen, indem wir die aktuelle Endcover-Position verwenden.
+    // Dies sollte in turn.js automatisch die Seite an die richtige Stelle schieben.
+    $(book).turn('addPage', `<div>${content}</div>`, priorCoverIndex);
+
+    let endCover1 = '<div class="hard"></div>'
+    let endCover2 = '<div class="hard"><img src="./media/matt.png" alt="" style="width: 100%;"></div>'
+
+    // Endcover wieder hinzufügen, damit sie am Ende bleiben.
+    $(book).turn('addPage', `<div>Seite</div>`, backCoverIndex);
+    $(book).turn('addPage', endCover1, backCoverIndex+1);
+    $(book).turn('addPage', endCover2, backCoverIndex + 2);
+}
